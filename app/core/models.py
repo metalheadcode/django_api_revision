540c1dbf892ccf925450235709dab72a9e35c9e0
Metal Head <azim1203@gmail.com>
2020-02-22 20:11:42 +0800

    created custom admin

--- app/core/models.py
+++ app/core/models.py
@@ -9,7 +9,7 @@ class UserManager(BaseUserManager):
         """Create and saves a new user"""
         if not email:
             raise ValueError('Users must have an email address')
-        user = self.model(email=self.normalize_email(email, **extra_fields))
+        user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self.db)
 
